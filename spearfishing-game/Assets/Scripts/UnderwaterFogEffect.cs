48c6d840354e25645eb07bf5e4305d6e16fa3663
Matthias <matthiashorgen@gmail.com>
2019-06-21 20:33:44 -1000

    Fishes

---
+++ spearfishing-game/Assets/Scripts/UnderwaterFogEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderwaterFogEffect : MonoBehaviour {
+    public float waterHeight;
+
+    private bool isUnderwater;
+    public Color fogColor;
+    
+    void Update() {
+        if ((transform.position.y < waterHeight) != isUnderwater) {
+            isUnderwater = transform.position.y < waterHeight;
+            if (isUnderwater) SetUnderwater();
+            if (!isUnderwater) SetNormal();
+        }
+    }
+
+    void SetNormal() {
+        RenderSettings.fog = false;
+    }
+
+    void SetUnderwater() {
+        RenderSettings.fog = true;
+        RenderSettings.fogColor = fogColor;
+        RenderSettings.fogDensity = 0.05f;
+    }
+}
