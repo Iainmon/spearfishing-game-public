10a6aa3c4ccb85241d2b30b4fc38d4893f0b1890
Matthias <matthiashorgen@gmail.com>
2019-06-22 10:34:09 -1000

    Moar FX

--- spearfishing-game/Assets/Scripts/CameraEffects.cs
+++ spearfishing-game/Assets/Scripts/CameraEffects.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PostProcessing;
 
-public class UnderwaterFogEffect : MonoBehaviour {
+public class CameraEffects : MonoBehaviour {
 
     public float waterHeight;
     public float maxDepth;
@@ -13,20 +14,26 @@ public class UnderwaterFogEffect : MonoBehaviour {
     public float shallowDensity;
     public float deepDensity;
 
+    public PostProcessingProfile SurfaceProfile;
+    public PostProcessingProfile UnderwaterProfile;
+
+
 
     void Start() {
         RenderSettings.fog = true;
     }
 
     void Update() {
-        if (GetDepth() <= 0) {
+        if (GetDepth() <= 0) { // Above water
             RenderSettings.fogDensity = 0;
+            GetComponent<PostProcessingBehaviour>().profile = SurfaceProfile;
         }
-        else {
+        else { // Underwater
             Color fogColor = Color.Lerp(shallowColor, deepColor, GetDepth() / maxDepth);
             float fogDensity = Mathf.Lerp(shallowDensity, deepDensity, GetDepth() / maxDepth);
             RenderSettings.fogColor = fogColor;
             RenderSettings.fogDensity = fogDensity;
+            GetComponent<UnityEngine.PostProcessing.PostProcessingBehaviour>().profile = UnderwaterProfile;
         }
     }
 
